d07fba314a98d2adc8a48497b72865f870dd5908
SpectralGT <atharvsingh1396@gmail.com>
2025-03-06 06:42:18 +0000

    added assets

--- backend/core/views.py
+++ backend/core/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(response):
-    return HttpResponse("<h1>Hello</h1>")
+def index(request):
+    return render(request, "index.html")
