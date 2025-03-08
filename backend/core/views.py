2131cd497fdf69fa5ad3274cdeeb4f346a0af452
SpectralGT <atharvsingh1396@gmail.com>
2025-03-08 15:09:45 +0000

    added form validation

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.core.checks import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.Info(request, "email already exist")
+                return redirect("signup.html")
+        else:
+            messages.Info(request, "Password not matching")
+            return redirect("signup.html")
     return render(request, "index.html")
 
 
